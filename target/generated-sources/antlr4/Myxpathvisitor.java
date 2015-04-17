9410efab56daa93c48273bff715af3b4431ba857
richard <miw072@ucsd.edu>
2015-04-17 14:46:48 -0700

    wmx

--- target/generated-sources/antlr4/Myxpathvisitor.java
+++ target/generated-sources/antlr4/Myxpathvisitor.java
@@ -16,6 +16,7 @@ public class Myxpathvisitor  extends xpathBaseVisitor<ArrayList<Node>> {
 	ArrayList<Node> currentList = new ArrayList<Node>();
 	DOMParser parser = new DOMParser();
 	
+	//absolute path slash
 	public ArrayList<Node> visitApsl(xpathParser.ApslContext ctx){
 		String xmlFile = ctx.ID().getText();
 		
@@ -31,6 +32,7 @@ public class Myxpathvisitor  extends xpathBaseVisitor<ArrayList<Node>> {
 		return visit(ctx.rp());
 	}
 	
+	//relative path tag
 	public ArrayList<Node> visitRptag(xpathParser.RptagContext ctx){
 		ArrayList<Node> result = new ArrayList<Node>();
 		String tagName = ctx.ID().getText();
@@ -48,14 +50,80 @@ public class Myxpathvisitor  extends xpathBaseVisitor<ArrayList<Node>> {
 		return result;
 	}
 	
+	//relative path slash
 	public ArrayList<Node> visitRpsrp(xpathParser.RpsrpContext ctx){
 		ArrayList<Node> result = new ArrayList<Node>();
 		result = visit(ctx.rp(0));
-		currentList= result;
-		result = visit(ctx.rp(1));
 		currentList = result;
+		result = visit(ctx.rp(1));
+		currentList = unique(result);
 		
+		return unique(result);
+	}
+	
+	//relative path current
+	public ArrayList<Node> visitRpcurr(xpathParser.RpcurrContext ctx){
+		return currentList;
+	}
+	
+	//relative path star(children)
+	public ArrayList<Node> visitRpchildren(xpathParser.RpchildrenContext ctx){
+		return getChildren();
+	}
+	
+	//relative path ..(parent)
+	public ArrayList<Node> visitRpparent(xpathParser.RpparentContext ctx){
+		return getParent();
+	}
+	
+	//relative path comma(and)
+	public ArrayList<Node> visitRpandrp(xpathParser.RpandrpContext ctx){
+		ArrayList<Node> result0 = new ArrayList<Node>();
+		ArrayList<Node> result1 = new ArrayList<Node>();
+		result0 = visit(ctx.rp(0));
+		result1 = visit(ctx.rp(1));
+		ArrayList<Node> result = new ArrayList<Node>(result0);
+		result.addAll(result1);
+		currentList = result;
 		return result;
+	}
+	
+	private ArrayList<Node> getChildren(){
+		ArrayList<Node> result = new ArrayList<Node>();
+		for (int i = 0; i < currentList.size(); i++){
+			NodeList curr = currentList.get(i).getChildNodes();
+			for (int j = 0; j < curr.getLength(); j++){
+				if (curr.item(j).getNodeType() == Node.ELEMENT_NODE){
+					result.add(curr.item(j));
+				}
+			}
+		}
+		currentList = result;
+		return result;
+	}
+	
+	private ArrayList<Node> getParent(){
+		ArrayList<Node> result = new ArrayList<Node>();
+		for (int i = 0; i < currentList.size(); i++){
+			Node curr = currentList.get(i).getParentNode(); // TO DO: if element?
+			result.add(curr);
+		}
+		currentList = unique(result);
+		return unique(result);
+	}
+	
+	
+	private ArrayList<Node> unique(ArrayList<Node> arr){
+		HashSet<Node> set = new HashSet<Node>();
+		for (int i = 0; i < arr.size(); i++ ){
+			Node n = arr.get(i);
+			if(set.contains(n)){
+				arr.remove(i);
+			}else{
+				set.add(n);
+			}
+		}
+		return arr;
 	}
 	
 	
@@ -74,8 +142,6 @@ public class Myxpathvisitor  extends xpathBaseVisitor<ArrayList<Node>> {
 		
 		for (int i = 0; i < rs.size(); i++){
 			System.out.println(rs.get(i).getNodeName());
-		}
-		
-		
+		}	
 	}
 }
